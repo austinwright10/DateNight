d466a2a6c71f8a44418d24a7c51f2b9487322804
Austin Wright <austwright10@gmail.com>
2024-09-12 18:11:09 -0600

    styling OTP component

--- DateNight/src/onboarding/OTP.tsx
+++ DateNight/src/onboarding/OTP.tsx
@@ -1,8 +1,20 @@
-import { Text } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { OtpInput } from 'react-native-otp-entry'
 
 export default function OTP() {
   return (
-    <OtpInput numberOfDigits={6} onTextChange={(text) => console.log(text)} />
+    <View style={styles.container}>
+      <Text>Enter the 6 digit code we sent to your number.</Text>
+      <OtpInput numberOfDigits={6} onTextChange={(text) => console.log(text)} />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
