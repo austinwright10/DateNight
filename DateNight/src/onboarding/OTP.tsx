332ddb8c8d146e8d2e70d4485ea8cbe071ebce63
Austin Wright <austwright10@gmail.com>
2024-09-12 18:07:58 -0600

    added OTP component

--- DateNight/src/onboarding/OTP.tsx
+++ DateNight/src/onboarding/OTP.tsx
@@ -1,5 +1,8 @@
 import { Text } from 'react-native'
+import { OtpInput } from 'react-native-otp-entry'
 
 export default function OTP() {
-  return <Text>hello</Text>
+  return (
+    <OtpInput numberOfDigits={6} onTextChange={(text) => console.log(text)} />
+  )
 }
