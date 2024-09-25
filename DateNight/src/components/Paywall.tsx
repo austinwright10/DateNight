8d94f952897794dad0470175f42615544eaa3a2a
Austin Wright <austwright10@gmail.com>
2024-09-24 20:22:08 -0600

    finally got icons working

--- DateNight/src/components/Paywall.tsx
+++ DateNight/src/components/Paywall.tsx
@@ -1,10 +1,12 @@
 import { Text, StyleSheet, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 export default function Paywall() {
   return (
     <View style={styles.container}>
       <View style={styles.paywallContainer}>
         <Text style={styles.header}>Start receiving your date nights!</Text>
         <View style={styles.subContainer}>
+          <Icon name='heart' size={16} color='#ff6666' />
           <Text style={styles.subHeader}>
             Unlock weekly date night recommendations based on your preferences
           </Text>
