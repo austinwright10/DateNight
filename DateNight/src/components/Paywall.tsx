1e454b3574497f1c225422e639c4608fb9b69819
Austin Wright <austwright10@gmail.com>
2024-09-24 19:32:36 -0600

    basic paywall so far

--- DateNight/src/components/Paywall.tsx
+++ DateNight/src/components/Paywall.tsx
@@ -1,4 +1,24 @@
-import { Text } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 export default function Paywall() {
-  return <Text>hi</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.paywallContainer}>
+        <Text>hi</Text>
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffcccc',
+  },
+  paywallContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+  },
+})
